a730998dfbef9a0231c6bda5a409c28d1af3d7b5
Dorian Burihabwa <dorian@burihabwa.com>
2014-09-16 15:42:35 +0200

    Basic station API retrieval
    Basic station API retrieval using the vlille XML API.

---
+++ src/main/java/com/burihabwa/vlille/Station.java
@@ -0,0 +1,75 @@
+package com.burihabwa.vlille;
+
+import java.util.Calendar;
+
+/**
+ * Created by dorian on 9/15/14.
+ */
+public class Station {
+    private int id;
+    private String address;
+    private int bikes;
+    private int emptySockets;
+    private Calendar lastUpdate;
+    private boolean creditCardTerminal;
+
+    public Station() {
+    }
+
+    public Station(int id, String address, int bikes, int emptySockets, Calendar lastUpdate, boolean creditCardTerminal) {
+        this.id = id;
+        this.address = address;
+        this.bikes = bikes;
+        this.emptySockets = emptySockets;
+        this.lastUpdate = lastUpdate;
+        this.creditCardTerminal = creditCardTerminal;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getBikes() {
+        return bikes;
+    }
+
+    public void setBikes(int bikes) {
+        this.bikes = bikes;
+    }
+
+    public int getEmptySockets() {
+        return emptySockets;
+    }
+
+    public void setEmptySockets(int emptySockets) {
+        this.emptySockets = emptySockets;
+    }
+
+    public Calendar getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Calendar lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public boolean hasCreditCardTerminal() {
+        return creditCardTerminal;
+    }
+
+    public void setCreditCardTerminal(boolean creditCardTerminal) {
+        this.creditCardTerminal = creditCardTerminal;
+    }
+}
---
+++ src/main/java/com/burihabwa/vlille/StationStore.java
@@ -0,0 +1,88 @@
+package com.burihabwa.vlille;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by dorian on 9/15/14.
+ */
+public class StationStore {
+    private static final String BASE_URL = "http://vlille.fr/stations/xml-station.aspx?borne=";
+
+    public Station getStation(final int id) {
+        Station station = null;
+        URL url = null;
+        HttpURLConnection connection = null;
+        InputStream is = null;
+        try {
+            url = new URL(BASE_URL + id);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            is = connection.getInputStream();
+            byte[] buffer = new byte[is.available()];
+            is.read(buffer);
+            String data = new String(buffer);
+            is.close();
+            //System.out.println(data);
+            station = parse(data);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return station;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return station;
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+            return station;
+        } catch (SAXException e) {
+            e.printStackTrace();
+            return station;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        station.setId(id);
+        return station;
+    }
+
+    public Station parse(final String data) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = dbFactory.newDocumentBuilder();
+        InputStream is = new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_16));
+        Document document = documentBuilder.parse(is);
+        is.close();
+
+        String address = document.getDocumentElement().getElementsByTagName("adress").item(0).getTextContent();
+        int bikes = Integer.parseInt(document.getDocumentElement().getElementsByTagName("bikes").item(0).getTextContent(), 10);
+        int attachs = Integer.parseInt(document.getDocumentElement().getElementsByTagName("attachs").item(0).getTextContent(), 10);
+        long lastupd = Long.parseLong(document.getDocumentElement().getElementsByTagName("lastupd").item(0).getTextContent().split("\\s")[0], 10) * 1000;
+
+        Calendar lastUpdate = new GregorianCalendar();
+        lastUpdate.setTimeInMillis(lastUpdate.getTimeInMillis() - lastupd);
+
+        Station station = new Station();
+        station.setAddress(address);
+        station.setBikes(bikes);
+        station.setEmptySockets(attachs);
+        station.setLastUpdate(lastUpdate);
+
+        return station;
+    }
+}
